fb23f9c8560cfe231b0b5f7ba621b5b43ab16cd6
Leo Ando <ando.reo420@gmail.com>
2021-06-10 23:16:30 +0900

    toBooleanStrictとtoBooleanStrictOrNullのサンプル実装

--- BooleanSample/src/main/kotlin/main.kt
+++ BooleanSample/src/main/kotlin/main.kt
@@ -1,3 +1,38 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    testBoolean_kotlin_1_4()
+    toBooleanStrict()
+    toBooleanStrictOrNull()
+}
+
+fun toBooleanStrictOrNull() {
+    println("=============toBooleanStrictOrNull-START============")
+    println("TRUE".toBooleanStrictOrNull())
+    println("FALSE".toBooleanStrictOrNull())
+    println("true".toBooleanStrictOrNull())
+    println("false".toBooleanStrictOrNull())
+    println("=============toBooleanStrictOrNull-END============")
+}
+
+fun toBooleanStrict() {
+    println("=============toBooleanStrict-START============")
+    runCatching {
+        println("TRUE".toBooleanStrict())
+    }.onFailure { println(it.localizedMessage) }
+    runCatching {
+        println("FALSE".toBooleanStrict())
+    }.onFailure { println(it.localizedMessage) }
+    println("true".toBooleanStrict())
+    println("false".toBooleanStrict())
+    println("=============toBooleanStrict-END============")
+}
+
+fun testBoolean_kotlin_1_4() {
+    println("=============testBoolean_kotlin_1_4-START============")
+
+    println("TRUE".toBoolean())
+    println("false".toBoolean())
+    println("tRuE".toBoolean())
+    println("1".toBoolean())
+    println(null.toBoolean())
+    println("=============testBoolean_kotlin_1_4-END============")
 }
