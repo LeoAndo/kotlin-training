97f071d4a041210e37bd14aec15bbc78f369a989
Leo Ando <ando.reo420@gmail.com>
2021-06-10 23:47:27 +0900

    Char型の値の数値変換(Unicode or number)

--- CovertTextSample/src/main/kotlin/main.kt
+++ CovertTextSample/src/main/kotlin/main.kt
@@ -2,6 +2,38 @@ import java.util.*
 
 fun main(args: Array<String>) {
     uppercaseTest()
+    convertCharToUnicode()
+}
+
+private const val UNICODE_NUM_ZERO = 51 // '1'のunicode 10進表記
+private const val BASE_NUMBER = 16 // 16進数
+fun convertCharToUnicode() {
+    println("===============convertCharToUnicode-START==============")
+    val char = '!'
+    val number = 'A'
+    println("char.code is : ${char.code}")
+    println("number.code is : ${number.code}")
+    println("Char(UNICODE_NUM_ZERO) is:  ${Char(UNICODE_NUM_ZERO)}")
+
+    runCatching {
+        println("char.digitToInt() is: " + char.digitToInt())
+    }.onFailure { if (it is IllegalArgumentException) println(it.localizedMessage) }
+    runCatching {
+        println("char.digitToInt(16) is: " + char.digitToInt(BASE_NUMBER))
+    }.onFailure { if (it is IllegalArgumentException) println(it.localizedMessage) }
+    println("char.digitToIntOrNull() is: " + char.digitToIntOrNull())
+    println("char.digitToIntOrNull(16) is: " + char.digitToIntOrNull(BASE_NUMBER))
+
+    runCatching {
+        println("number.digitToInt() is: " + number.digitToInt())
+    }.onFailure { if (it is IllegalArgumentException) println(it.localizedMessage) }
+    runCatching {
+        println("number.digitToInt(16) is: " + number.digitToInt(BASE_NUMBER))
+    }.onFailure { if (it is IllegalArgumentException) println(it.localizedMessage) }
+    println("number.digitToIntOrNull() is: " + number.digitToIntOrNull())
+    println("number.digitToIntOrNull(16) is: " + number.digitToIntOrNull(BASE_NUMBER))
+
+    println("===============convertCharToUnicode-END==============")
 }
 
 fun uppercaseTest() {
