3f9d7b8a361a89716c8370c6ac7f1ffe8d94686e
Leo Ando <ando.reo420@gmail.com>
2021-06-11 15:44:18 +0900

    inline value classes

--- InlineValueClassesSample/src/main/kotlin/main.kt
+++ InlineValueClassesSample/src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    payMoney(500) // これだと通貨がわからない.
+    payMoney(Cents(200))
+    payMoney(Cents2(100))
+}
+
+private fun payMoney(price: Int){
+    println("payMoney: $price")
+}
+
+private fun payMoney(price: Cents){
+    println("payMoney: ${price.amount}")
+}
+
+private fun payMoney(price: Cents2){
+    println("payMoney: ${price.amount}")
 }
