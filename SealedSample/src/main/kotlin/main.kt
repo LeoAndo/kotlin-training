43f2cfda229841b67f9bd4977ba3eda84bf86a78
Leo Ando <ando.reo420@gmail.com>
2021-06-11 15:09:48 +0900

    Support for sealed interfaces & sealed classes improvements

--- SealedSample/src/main/kotlin/main.kt
+++ SealedSample/src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
+import Utils.safeApiCall
+import java.util.*
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    print("input number: ")
+    val scan = Scanner(System.`in`)
+    val response = safeApiCall(onAction = { scan.nextInt() })
+    when (response) {
+        is Success -> print("success: ${response.data}")
+        is InputMismatchError -> print("input mismatch error!")
+        is Failure -> print(response.exception.localizedMessage)
+    }
 }
