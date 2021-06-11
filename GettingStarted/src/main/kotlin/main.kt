b829c881e908d89a3e5fdab45daf62db90b349a3
Leo Ando <ando.reo420@gmail.com>
2021-06-11 16:28:50 +0900

    #7 toIntOrNullを使ったサンプル

--- GettingStarted/src/main/kotlin/main.kt
+++ GettingStarted/src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    testToIntOrNull()
+}
+
+fun testToIntOrNull() {
+    val result1 = "1".toIntOrNull()
+    println(result1)
+    val result2 = "abc".toIntOrNull() // parse error
+    print(result2)
 }
